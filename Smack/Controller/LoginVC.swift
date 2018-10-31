d0e0a65dbbdd2f56aba88fafb280fdaa9a9d4a66
fguardiola <fernando.guardiola.888@gmail.com>
2018-10-31 17:04:17 +0100

    End of lesson 4. Login UI

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by 67621177 on 31/10/2018.
+//  Copyright © 2018 67621177. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Smack/Utiils/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by 67621177 on 31/10/2018.
+//  Copyright © 2018 67621177. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
